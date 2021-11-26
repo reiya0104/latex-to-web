96d0814123acd50e1dce3e31c8d5a1446e7b5c3e
Reiya <reiya0104@ezweb.ne.jp>
2021-11-26 16:25:01 +0900

    Add guard.ts

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,10 @@
 import Vue from 'vue';
 import * as VueRouter from 'vue-router';
 import { createRouter, createWebHistory } from 'vue-router'
+import { authorize } from '@/router/guard';
 // HomeビューとAboutビューのインポート
-import Home from './views/Home.vue';
-import About from './views/About.vue';
+import Home from '@/views/Home.vue';
+import About from '@/views/About.vue';
 
 const routes = [
   {
